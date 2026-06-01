0c599d3d7c62e89052a692f7c53857a31a2fe54c
Pablo <pablopiovan94@gmail.com>
2026-06-01 17:48:17 -0300

    Bcrypt a las passwords

--- Sistema de Reservas/backend/src/controllers/auth/AuthController.js
+++ Sistema de Reservas/backend/src/controllers/auth/AuthController.js
@@ -1,4 +1,5 @@
 import { Usuario } from "../../models/Usuario.js";
+import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
   try {
@@ -30,9 +31,10 @@ export const login = async (req, res) => {
     }
 
     // Comparar contraseña
-    if (usuario.password !== password) {
+    const coincide = await bcrypt.compare(password, usuario.password);
+    if(!coincide){
       return res.status(401).json({
-        mensaje: "Contraseña incorrecta",
+        mensaje: "Password incorrecta"
       });
     }
 
@@ -54,4 +56,67 @@ export const login = async (req, res) => {
       mensaje: "Error interno del servidor",
     });
   }
+};
+
+export const register = async (req, res) => {
+    try {
+
+        const {
+            nombre,
+            apellido,
+            email,
+            password,
+            telefono
+        } = req.body;
+
+        if (
+            !nombre ||
+            !apellido ||
+            !email ||
+            !password
+        ) {
+            return res.status(400).json({
+                mensaje: "Todos los campos son obligatorios"
+            });
+        }
+
+        const existeUsuario = await Usuario.findOne({
+            where: { email }
+        });
+
+        if (existeUsuario) {
+            return res.status(400).json({
+                mensaje: "El email ya está registrado"
+            });
+        }
+
+        const passwordHash = await bcrypt.hash(
+            password,
+            10
+        );
+
+        const usuario = await Usuario.create({
+            id_rol: 3,
+            nombre,
+            apellido,
+            email,
+            password: passwordHash,
+            telefono,
+            activo: true
+        });
+
+        return res.status(201).json({
+            mensaje: "Usuario registrado correctamente",
+            id: usuario.id_usuario
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        return res.status(500).json({
+            mensaje: "Error interno"
+        });
+
+    }
 };
--- Sistema de Reservas/backend/src/routes/authRoutes.js
+++ Sistema de Reservas/backend/src/routes/authRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { login } from "../controllers/auth/AuthController.js";
+import { login, register } from "../controllers/auth/AuthController.js";
 
 const router = Router();
 
 router.post("/login", login);
+router.post("/register", register);
 
+router.get("/test", (req, res) => {
+    res.json({
+        mensaje: "Funciona"
+    });
+});
 export default router;
